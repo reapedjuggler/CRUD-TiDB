8dfaf731b685e6c844c2644b069c065d58d5d01b
ShrutiGarg89 <gargshruti3001@gmail.com>
2023-07-17 13:14:05 +0530

    Comments resolved

--- utils/dbQuery.js
+++ utils/dbQuery.js
@@ -16,27 +16,17 @@ const createTableQuery = `
 const selectTableQuery = `
             SELECT * FROM images
         `
-const insertTableQuery = `INSERT INTO images VALUES('${image}', '${imageName}')`;
+//const insertTableQuery = `INSERT INTO images VALUES('${image}', '${imageName}')`;
+
+function insertTable(image,imageName){
+    const insertTableQuery = `INSERT INTO images VALUES('image','imageName)`;
+    console.log(insertTableQuery);
+}
+
 const showTables = `
             show tables;
         `
 const dropTableQuery = "DROP TABLE images"
-
-let getRandomMeme = () => {
-    try {
-
-    } catch (err) {
-
-    }
-}
-
-let createTable = () => {
-    try {
-        connection.query("CREATE TABLEes (id INTEGER, fileName VARCHAR)")
-    } catch (err) {
-        console.log("Error in createTable");
-    }
-}
 
 module.exports = exports = {
     insertQuery,
